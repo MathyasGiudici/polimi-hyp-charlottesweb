2be172884339b95a817296bc94f162473a2ab685
sissifranz <silvia1.franzini@mail.polimi.it>
2019-04-18 22:53:22 +0200

    books database

--- other/service/fillings/BooksData.js
+++ other/service/fillings/BooksData.js
@@ -12,12 +12,12 @@ exports.giveMeData = function() {
       photo : "",
       type : "paper",
       pubbDate : "2012-4-10",
-      genre : "kids",
+      genre : "children",
       theme : "friendship",
       //similarTo : [ "9788867024766", "9788804606246" ],
       status : "available",
       ourFavorite : true,
-      bestSelling : true,
+      bestSelling : false,
       value : 8.04,
       currency : "euro"
     },
@@ -31,7 +31,7 @@ exports.giveMeData = function() {
       photo : "",
       type : "paper",
       pubbDate : "2000-5-15",
-      genre : "kids",
+      genre : "children",
       theme : "friendship",
       //similarTo : [ "9788867024766", "9788804606246" ],
       status : "available",
@@ -50,8 +50,8 @@ exports.giveMeData = function() {
       photo : "",
       type : "paper",
       pubbDate : "2017-3-2",
-      genre : "kids",
-      theme : "equality",
+      genre : "children",
+      theme : "freedom",
       //similarTo : [ "9788867024766", "9788804606246" ],
       status : "available",
       ourFavorite : false,
@@ -76,6 +76,101 @@ exports.giveMeData = function() {
       ourFavorite : false,
       bestSelling : true,
       value : 18.56,
+      currency : "euro"
+    },
+    {
+      isbn : "9788491051329",
+      title : "Pride and Prejudice",
+      description : "When Elizabeth Bennet first meets eligible bachelor Fitzwilliam Darcy, she thinks him arrogant and conceited; he is indifferent to her good looks and lively mind. When she later discovers that Darcy has involved himself in the troubled relationship between his friend Bingley and her beloved sister Jane, she is determined to dislike him more.",
+      interview : "The most superb comedy of manners of Regency England",
+      numOfPages : 428,
+      //author : "authorA001",
+      photo : "",
+      type : "paper",
+      pubbDate : "2017-6-20",
+      genre : "romance",
+      theme : "love",
+      //similarTo : [ "9788867024766", "9788804606246" ],
+      status : "available",
+      ourFavorite : false,
+      bestSelling : true,
+      value : 10.03,
+      currency : "euro"
+    },
+    {
+      isbn : "9780300039504",
+      title : "The Psychology of Love",
+      description : "Originally written in 1988, The Psychology of Love (public library) is an anthology of 16 academic, though highly readable, papers dissecting various aspects of love. The collection is divided into five parts, each focusing on a specific facet of understanding love, from global theories that explain the phenomenon, to the psychology of relationship maintenance, to a critical overview of the field of love research.",
+      interview : "Perhaps still the best psychological treatment of love",
+      numOfPages : 395,
+      //author : "authorA001",
+      photo : "",
+      type : "paper",
+      pubbDate : "1988-7-1",
+      genre : "romance",
+      theme : "love",
+      //similarTo : [ "9788867024766", "9788804606246" ],
+      status : "available",
+      ourFavorite : false,
+      bestSelling : false,
+      value : 10.03,
+      currency : "euro"
+    },
+    {
+      isbn : "9781943818839",
+      title : "Walk In The Fire (Judah Cannon)",
+      description : "Life hasn’t gotten any easier for Judah Cannon. He may have survived the fiery showdown between his father, the tyrannical Pentecostal preacher Sister Tulah, and the Scorpions outlaw motorcycle club, but now Judah and Ramey, the love of his life turned partner in crime, are facing new and more dangerous adversaries. It will take all of their cunning and courage, their faith in one another and some unexpected help to give them even a shot of making it out alive.",
+      interview : "“Steph Post is a great new discovery. Her stories carry a dark pulse that keeps the perfect beat in a world where people put everything they’ve got on the line. Walk In The Fire is going to put Steph Post on the map.” ―Michael Connelly ",
+      numOfPages : 336,
+      //author : "authorA001",
+      photo : "",
+      type : "paper",
+      pubbDate : "2018-1-16",
+      genre : "thriller",
+      theme : "justice",
+      //similarTo : [ "9788867024766", "9788804606246" ],
+      status : "available",
+      ourFavorite : false,
+      bestSelling : true,
+      value : 20.15,
+      currency : "euro"
+    },
+    {
+      isbn : "9780316327626",
+      title : "The Smack: A Novel",
+      description : "Rowan Petty is a conman down on his luck. He's flat broke, living out of cheap hotels, and wondering how it all went wrong. His car quits on him in Reno, and he takes a job there on the bottom rung of a lousy phone scam. When he's not swindling lonely widows, he tries to turn nickels into dimes at the poker table. One snowy night, he crosses paths with a sweet-talking hooker who's tired of the streets, and sparks fly.",
+      interview : "'That rare thriller that will jack your pulse even as it breaks your heart.'--Adam Sternbergh, author of Shovel Ready",
+      numOfPages : 368,
+      //author : "authorA001",
+      photo : "",
+      type : "paper",
+      pubbDate : "2017-7-18",
+      genre : "thriller",
+      theme : "death",
+      //similarTo : [ "9788867024766", "9788804606246" ],
+      status : "available",
+      ourFavorite : false,
+      bestSelling : false,
+      value : 17.67,
+      currency : "euro"
+    },
+    {
+      isbn : " 9781786498229",
+      title : "The Silver Road",
+      description : "Three years ago, Lelle's daughter went missing in a remote part of Northern Sweden. Lelle has spent the intervening summers driving the Silver Road under the midnight sun, frantically searching for his lost daughter, for himself and for redemption",
+      interview : "'Haunting, intoxicating' Ali Land, author of Good Me, Bad Me",
+      numOfPages : 352,
+      //author : "authorA001",
+      photo : "",
+      type : "paper",
+      pubbDate : "2019-3-07",
+      genre : "thriller",
+      theme : "justice",
+      //similarTo : [ "9788867024766", "9788804606246" ],
+      status : "available",
+      ourFavorite : false,
+      bestSelling : false,
+      value : 10.50,
       currency : "euro"
     }
   ];
@@ -106,7 +201,55 @@ exports.giveMeSimilars = function() {
     { //symmetric relation
       isbn2: "9780061124952",
       isbn1: "9780120420476"
-    }
+    },
+    {
+      isbn1: "9781538728529",
+      isbn2: "9788491051329"
+    },
+    { //symmetric relation
+      isbn2: "9788491051329",
+      isbn1: "9781538728529"
+    },
+    {
+      isbn1: "9781538728529",
+      isbn2: "9780300039504"
+    },
+    { //symmetric relation
+      isbn2: "9780300039504",
+      isbn1: "9781538728529"
+    },
+    {
+      isbn1: "9780300039504",
+      isbn2: "9788491051329"
+    },
+    { //symmetric relation
+      isbn2: "9780300039504",
+      isbn1: "9781538728529"
+    },
+    {
+      isbn1: "9781943818839",
+      isbn2: "9780316327626"
+    },
+    { //symmetric relation
+      isbn2: "9780316327626",
+      isbn1: "9781943818839"
+    },
+    {
+      isbn1: "9781943818839",
+      isbn2: "9781786498229"
+    },
+    { //symmetric relation
+      isbn2: "9781786498229",
+      isbn1: "9781943818839"
+    },
+    {
+      isbn1: "9780316327626",
+      isbn2: "9781786498229"
+    },
+    { //symmetric relation
+      isbn2: "9781786498229",
+      isbn1: "9780316327626"
+    },
   ];
 }
 
@@ -136,6 +279,41 @@ exports.giveMeAuthors = function() {
      {
       isbn: "9781538728529",
       author1: "Nicholas Sparks",
+      author2: "",
+      author3: "",
+      author4: ""
+    },
+    {
+      isbn: "9788491051329",
+      author1: "Jane Austen",
+      author2: "",
+      author3: "",
+      author4: ""
+    },
+    {
+      isbn: "9780300039504",
+      author1: "Robert J. Sternberg",
+      author2: "Michael L. Barnes",
+      author3: "",
+      author4: ""
+    },
+    {
+      isbn: "9781943818839",
+      author1: "Steph Post",
+      author2: "",
+      author3: "",
+      author4: ""
+    },
+    {
+      isbn: "9780316327626",
+      author1: "Richard Lange",
+      author2: "",
+      author3: "",
+      author4: ""
+    },
+    {
+      isbn: "9781786498229",
+      author1: "Stina Jackson",
       author2: "",
       author3: "",
       author4: ""
---
+++ other/service/fillings/EventsData.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports.dataEvents = function() {
+  return [
+    
+  ];
+}
