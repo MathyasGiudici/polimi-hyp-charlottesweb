497d0c39e4590d675e1fd82b85680806ac3009cd
MathyasGiudici <mathyas.giudici@mail.polimi.it>
2019-06-01 18:54:43 +0200

    Fixed: authors

--- public/startbootstrap-business-casual/js/authorsFunctions.js
+++ public/startbootstrap-business-casual/js/authorsFunctions.js
@@ -2,30 +2,48 @@
 let baseUrl = "https://webserver-test-polimi.herokuapp.com/api/";
 
 $(document).ready(function(){
-  console.log("Authors Loading");
-
   $.ajax({
     url: baseUrl + "authors",
     dataType: "json",
     success:function(data){
+      let toAppend = '';
+      let step = 1;
       for(let i = 0; i< data.length; i++){
-        let toAppend;
-
         //photo
-        let beforePhoto = '<div class="col-md-4 col-xs-12 bestseller-item"><div class="align-self-center mr-3"><img class="align-self-start" src="';
+        let beforePhoto = '<div class="col-md-4 col-xs-12 bestseller-item"><div class="align-self-center mr-3" id="threeBookInRow">' +
+        '<img class="align-self-start" alt="image of author: ' + data[i].id  + '" src="';
         let beforeTitle = '" onClick="handleAuthorClick('+"'" + data[i].id +"'" +');" style="width: 70%" alt=""><div class="cta descr-padding"><h5 class="mt-0" id="AuthorsName">';
         //name and surname of the author
-        let beforeDescription = '</h5>';
-        let afterDescription = '</div></div></div></div>';
+        let end = '</h5></div></div></div>';
 
-        if(i%3==0 && i!=0 && i!=data.length-1){
-         newRow='<div class="small-padding"></div>';
-         toAppend=newRow+beforePhoto + data[i].photo + beforeTitle + data[i].name+' '+data[i].surname+ beforeDescription + afterDescription ;
-        }
-         toAppend = beforePhoto + data[i].photo + beforeTitle + data[i].name+' '+data[i].surname+ beforeDescription + afterDescription ;
+        toAppend = toAppend + beforePhoto + data[i].photo + beforeTitle + data[i].name+' '+data[i].surname+ end ;
 
+         if(step == 1){
+           toAppend = '<div class="row ml-3" style="padding-bottom: 20px;">' + toAppend;
+           step = 2;
 
-        $("#authors").append(toAppend);
+           if((data.length-1) == i){
+             toAppend = toAppend + "</div>";
+             $("#authorsContainer").append(toAppend);
+             toAppend = "";
+             step = 1;
+           }
+         }
+         else{
+           if((data.length-1) == i){
+             step = 3;
+           }
+
+           if(step == 2){
+               step = 3;
+           }
+           else{
+               toAppend = toAppend + "</div>";
+               $("#authorsContainer").append(toAppend);
+               toAppend = "";
+               step = 1;
+           }
+         }
       }
     },
     error:function(jqXHR, textStatus, errorThrown){
